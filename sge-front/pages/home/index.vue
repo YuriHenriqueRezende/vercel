5bcecc8a98c23d10f9dc4a901ec09f0a45a96f67
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-03-21 20:25:34 -0300

    commit

---
+++ sge-front/pages/home/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const { signOut } = useAuth();
+
+definePageMeta({
+    middleware: 'auth'
+})
+
+const logout = ()=> {
+    signOut({redirect: false})
+    .then(()=> {
+        console.log("loged");
+        navigateTo("/");
+    })
+    .catch((error)=>{
+        console.log("Erros", error);
+
+    });
+}
+
+
+</script>
+
+<template>
+    <div>
+        <h1>Minha Tela Home!!!</h1> 
+        <button @click="logout" class="customButton">SAIR</button>   
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
